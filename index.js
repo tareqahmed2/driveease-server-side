157667934349076ab50307f403a990cb2a9ff0ab
tareq ahmed <tareqahmed347396@gmail.com>
2024-12-24 02:09:49 +0600

    added delete method for mybooking page

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 // const multer = require("multer");
 // const path = require("path");
+const app = express();
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const app = express();
 app.use(cors());
 
 app.use(express.json());
@@ -46,6 +47,7 @@ async function run() {
     });
     app.post("/all-bookings", async (req, res) => {
       const newCar = req.body;
+
       const result = await allBookingCollection.insertOne(newCar);
       res.send(result);
     });
@@ -64,6 +66,13 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await allCarsCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/deleteBooked/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+      const result = await allBookingCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
